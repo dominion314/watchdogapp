3c37d476b4e900948a3d878a6c86f4648284faa0
dominion314 <dominickhrndz314@gmail.com>
2022-11-17 21:53:40 -0600

    Basic components and purpose texts added

---
+++ watchdog/src/App.vue
@@ -0,0 +1,12 @@
+<!--This is our app.vue componenet which will load the layout of our app in the router-view. This connected to the /layouts folder.-->
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
---
+++ watchdog/src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<!--This comes from the MainLayout.vue file and is the home page of our app.
+This can pull from the /public folder for images or css files-->
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
